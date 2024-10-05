53c8509a82e8d55505264b5809bf5d717355bfee
Guram Soselia <guram.soselia@gmail.com>
2024-10-05 20:15:53 +0400

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StreamingGateway } from './streaming.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, StreamingGateway],
 })
